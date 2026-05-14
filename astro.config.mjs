4609ce07ce1d13d0460df0b921ff9b8d8efff482
Luc Perkins <lucperkins@gmail.com>
2026-05-14 11:22:50 -0700

    Fix dependency issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,44 +2,46 @@ import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
-import { defineConfig } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
 import remarkEmoji from "remark-emoji";
 import remarkHeadingId from "remark-heading-id";
 
 export default defineConfig({
-  integrations: [
-    alpinejs({
-      entrypoint: "./src/entrypoint",
-    }),
-    expressiveCode(),
-    icon(),
-    mdx({
-      //gfm: true,
-      remarkPlugins: [remarkEmoji, remarkHeadingId],
-    }),
-    sitemap(),
-    tailwind(),
-    react(),
-  ],
-  markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        { rel: ["nofollow noopener noreferrer"], target: "_blank" },
-      ],
-    ],
+	integrations: [
+		alpinejs({
+			entrypoint: "./src/entrypoint",
+		}),
+		expressiveCode(),
+		icon(),
+		mdx({
+			//gfm: true,
+			remarkPlugins: [remarkEmoji, remarkHeadingId],
+		}),
+		sitemap(),
+		react(),
+	],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{ rel: ["nofollow noopener noreferrer"], target: "_blank" },
+			],
+		],
+	},
+	prefetch: {
+		prefetchAll: true,
+		defaultStrategy: "hover",
+	},
+	server: {
+		open: true,
+		port: 3000,
+	},
+	site: "https://zero-to-nix.com",
+  vite: {
+    plugins: [tailwind()],
   },
-  prefetch: {
-    prefetchAll: true,
-    defaultStrategy: "hover",
-  },
-  server: {
-    open: true,
-    port: 3000,
-  },
-  site: "https://zero-to-nix.com",
 });
